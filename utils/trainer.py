9ed2826ae21a6a6caaafdab501456daec9dc3d1a
Hirotaka Kawashima <k.hirotaka@me.com>
2019-08-13 00:10:45 +0900

    typo

--- utils/trainer.py
+++ utils/trainer.py
@@ -30,7 +30,7 @@ class NeuralNetworkClassifier:
         def forward(self, x):
             ...
 
-    optimizer_config = {"lr": 0.001, "betas":( 0.9, 0.999), "eps": 1e-08}
+    optimizer_config = {"lr": 0.001, "betas": (0.9, 0.999), "eps": 1e-08}
     comet_config = {}
 
     train_val_loader = {
