e50df29de18fb7d3dae461f32bcc5b4997ce0b32
Hirotaka Kawashima <k.hirotaka@me.com>
2019-08-29 20:17:49 +0900

    Update dataset.py

--- Typhoon/utils/dataset.py
+++ Typhoon/utils/dataset.py
@@ -107,18 +107,18 @@ def load_har(raw=True):
 
         for mode in types:
             data = load_raw_data(mode)
-            target = pd.read_csv()
+            target = pd.read_csv(save_dir + mode + "/y_" + mode + ".txt", header=None, delim_whitespace=True).values
 
             raw_datasets.append((data, target))
 
         (x_train, y_train), (x_test, y_test) = raw_datasets[0], raw_datasets[1]
 
     else:
-        x_train = pd.read_csv()
-        y_train = pd.read_csv()
+        x_train = pd.read_csv(save_dir+"train/X_train.txt", header=None, delim_whitespace=True).values
+        y_train = pd.read_csv(save_dir + "train/y_train.txt", header=None, delim_whitespace=True).values
 
-        x_test = pd.read_csv()
-        y_test = pd.read_csv()
+        x_test = pd.read_csv(save_dir + "test/X_test.txt", header=None, delim_whitespace=True).values
+        y_test = pd.read_csv(save_dir + "test/y_test.txt", header=None, delim_whitespace=True).values
 
     return (x_train, y_train), (x_test, y_test)
 
