778464296bab53f132ef3283c82011b1e1f067f0
Hirotaka Kawashima <k.hirotaka@me.com>
2019-09-05 09:16:03 +0900

    Update train_mhealth.py

--- experiments/train_mhealth.py
+++ experiments/train_mhealth.py
@@ -7,16 +7,16 @@ from torch.backends import cudnn as cudnn
 from torch.utils.data import DataLoader
 
 from Typhoon.utils.dataset import MHealth
-from Typhoon.core.model import Typhoon
+from Typhoon.core.model import TyphoonClassifier
 from Typhoon.utils.trainer import NeuralNetworkClassifier
 
 torch.manual_seed(0)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-train_ds = MHealth([1, 2, 3, 4, 5, 6, 7, 8], 256)
-val_ds = MHealth([9], 256)
-test_ds = MHealth([10], 256)
+train_ds = MHealth([1, 2, 3, 4, 5, 6, 7], 256)
+val_ds = MHealth([8], 256)
+test_ds = MHealth([9, 10], 256)
 
 train_loader = DataLoader(train_ds, batch_size=128, shuffle=False)
 val_loader = DataLoader(val_ds, batch_size=128, shuffle=False)
@@ -30,7 +30,7 @@ num_class = 12
 
 
 clf = NeuralNetworkClassifier(
-    Typhoon(in_feature, seq_len, n_heads, factor, num_class, num_layers=6, d_model=256, dropout_rate=0.5),
+    TyphoonClassifier(in_feature, seq_len, n_heads, factor, num_class, num_layers=6, d_model=256, dropout_rate=0.5),
     nn.CrossEntropyLoss(),
     optim.Adam, {"lr": 0.000001, "betas": (0.9, 0.98), "eps": 4e-09, "weight_decay": 5e-4}, Experiment()
 )
